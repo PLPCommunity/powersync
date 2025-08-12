d454a5280a3c1a0ba78de7f4361020ee87471bc3
Evans-mutuku <evansnyamai98@gmail.com>
2025-08-12 14:16:39 +0300

    fixing ui

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import React, { Suspense} from "react";
+// import React, { Suspense} from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { HomePage } from "./Components/HomePage";
 import { BoardCanvas } from "./routes/BoardCanvas";
