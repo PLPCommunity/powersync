2e868c0a10c4e5b49d4b065e3db22715774a8bda
Evans-mutuku <evansnyamai98@gmail.com>
2025-08-23 19:30:25 +0300

    adding a welcome email

--- front-end/src/Components/Login.jsx
+++ front-end/src/Components/Login.jsx
@@ -2,7 +2,8 @@
 
 import { useEffect } from "react";
 // import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
-// import googleIcon from "../assets/google-icon.png";
+import googleIcon from "../assets/google-icon.png";
+import github from "../assets/github.png";
 import { auth, gitProvider, provider } from "../utils/firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout, selectUser } from "../features/userSlice";
@@ -100,7 +101,7 @@ const Login = () => {
         <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 md:mt-24 opacity-100 ease-out transition-all sm:max-w-4xl w-full m-3 mx-auto">
           <div className=" bg-white mx-auto w-full md:w-4/6 rounded-xl md:shadow-md md:border-2 mt-28 md:mt-24">
             <div className="flex justify-between items-center py-3 px-4 border-b">
-              <h3 className="font-bold text-gray-900">Progskill</h3>
+              <h3 className="font-bold text-gray-900">Mossara</h3>
               <button
                 type="button"
                 className="hs-dropdown-toggle inline-flex flex-shrink-0 justify-center items-center h-8 w-8 rounded-md text-gray-500 hover:text-gray-400 transition-all text-sm cursor-pointer"
@@ -113,27 +114,28 @@ const Login = () => {
 
             <div className="my-10 w-4/5 md:p-5 mx-auto items-center ">
               <h1 className="text-2xl text-center text-gray-800 font-bold">
-                Welcome to Progskill
+                Welcome to Mossara
               </h1>
 
               <section className=" w-full mx-auto mt-5  sm:p-5 md:p-5 lg:p-6 index-50 bg-white space-y-5 ">
                 <div
                   onClick={googleSignIn}
-                  className="flex items-center justify-center mx-auto w-full text-yellow-600 border border-gray-500 rounded-full cursor-pointer mt-4"
+                  className="flex items-center justify-center mx-auto w-full text-yellow-600 font-semibold border border-gray-500 rounded-full cursor-pointer mt-4"
                 >
-                  {/* <img src={googleIcon} className="w-12" alt="" /> */}
+                  <img src={googleIcon} className="w-14" alt="" />
                   <p className="">Sign up with Google </p>
                 </div>
                 <div
                   onClick={githubSignUp}
-                  className="flex items-center justify-center font-serif mx-auto w-full p-2 border border-gray-500 rounded-full cursor-pointer mt-4"
+                  className="flex items-center justify-center font-serif mx-auto w-full p-2 py-3 border font-semibold border-gray-500 rounded-full cursor-pointer mt-4"
                 >
+                  <img src={github} className="w-6" alt="" />
                   {/* <GitHubIcon className="w-24" /> */}
                   <p className="ml-5 ">Sign up with GitHub </p>
                 </div>
               </section>
               <p className="font-light text-xs text-center mt-8">
-                By clicking “Sign up” you agree to Progskill’s{" "}
+                By clicking “Sign up” you agree to Mossara’s{" "}
                 <a
                   className="underline"
                   href="https://progskill.com/terms"
